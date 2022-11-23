7950426ea65c007dda5aedb4057cd5e1cf32da23
james <james.bennett@bjss.com>
2022-11-23 23:37:36 +0000

    Remove unused import in webpack config

--- lambda/webhook/webpack.config.js
+++ lambda/webhook/webpack.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
